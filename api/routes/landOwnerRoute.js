30ea12b738fd344eb003a8f459a2438a6847b9e4
afreediz <afreedisulfiker@gmail.com>
2024-04-29 17:41:38 +0530

    frontend base set, now onto backend socket

--- api/routes/landOwnerRoute.js
+++ api/routes/landOwnerRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const landOwnerController = require("../controllers/landOwnerController")
 const { isAuth } = require("../middlewares/isAuth")
 
-router.get("/get-landOnwer", isAuth, landOwnerController.getLandOwner)
+router.get("/get-landOwner", isAuth, landOwnerController.getLandOwner)
 router.post("/login", )
 router.post("/register")
 
