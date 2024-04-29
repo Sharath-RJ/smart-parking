66eee8f85170188e6bfffb5c4aa29f35ea42b5de
Sharath <sharathj116@gmail.com>
2024-04-29 17:15:57 +0530

    still eror

--- api/routes/locationRoute.js
+++ api/routes/locationRoute.js
@@ -2,8 +2,8 @@ const express = require("express")
 const router = express.Router()
 const locationController= require("../controllers/locationController")
 
-router.get("/update/parking-status", locationController.updateParkingStatus)
-router.get("/parking-slots", locationController.getParkingSlots)
+// router.get("/update/parking-status", locationController.updateParkingStatus)
+// router.get("/parking-slots", locationController.getParkingSlots)
 
 
 module.exports = router
